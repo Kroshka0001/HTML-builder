d99cdefec5a26c64ef8d7346f0c5099e9e194321
Yuliya Laptsinskaya <89513166+Kroshka0001@users.noreply.github.com>
2023-05-08 23:48:57 +0300

    feat: add  completed task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const path = require("path");
+const fs = require("fs").promises;
 
+async function copyDir(src, dest) {
+
+  const entries = await fs.readdir(src, {withFileTypes: true});
+
+  await fs.rm(dest, {force: true, recursive: true});
+  await fs.mkdir(dest, {recursive: true});
+
+  for (let entry of entries) {
+
+    if (entry.isDirectory()) {
+
+      await copyDir(path.join(src, entry.name), path.join(dest, entry.name));
+
+    } else {
+
+      await fs.copyFile(path.join(src, entry.name), path.join(dest, entry.name));
+
+    }
+
+  }
+
+}
+
+copyDir(path.join(__dirname, "files"), path.join(__dirname, "files-copy"));
